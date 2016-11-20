efbc669fc559aa45269165b0d6b3842b2741ec33
MpaK <mrak69@gmail.com>
2016-11-20 17:36:09 +0700

    sign in automatically after user confirm

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,6 +2,19 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
   after_action :set_profiles, only: [:show]
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      # respond_with_navigational(resource){ sign_in_and_redirect after_confirmation_path_for(resource_name, resource) }
+      respond_with_navigational(resource){ sign_in_and_redirect resource }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+
   private
 
   def set_profiles
